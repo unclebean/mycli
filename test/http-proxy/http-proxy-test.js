1273aaddac6911ab1c3900dd1e157693ac93817f
Hugh <unclebean2013@gmail.com>
2015-11-06 00:13:01 +0800

    unit test

--- test/http-proxy/http-proxy-test.js
+++ test/http-proxy/http-proxy-test.js
@@ -9,9 +9,10 @@ var expect = chai.expect,
 var http = require('http');
 var https = require('https');
 var HTTPProxy = require("../../src/http-proxy/");
+var Datastore = require('nedb');
 
 describe('HTTPProxy', function(){
-  var _options = null;
+  var _options = null, db;
   beforeEach(function(){
     _options = {
       "server":{
@@ -24,6 +25,9 @@ describe('HTTPProxy', function(){
         "port":80
       }
     };
+    db = new Datastore({ filename: './proxyDB' });
+    db.loadDatabase(function (err) {});
+
   });
   it('should create http server', function(){
     sinon.stub(http, 'createServer', function(){
@@ -32,5 +36,46 @@ describe('HTTPProxy', function(){
     HTTPProxy.startup(_options);
     sinon.assert.calledOnce(http.createServer);
     http.createServer.restore();
+  });
+  it('should call db.update', function(){
+    var spy = sinon.spy(db, 'update');
+    HTTPProxy._insertOrUpdate("/mock", "{'test':'mock'}", {});
+    spy.called;
+  });
+  describe('performRequest', function(){
+    it('give "HTTPS" should call _createHTTPSRequest', function(){
+      sinon.stub(HTTPProxy, '_createHTTPSRequest', function(){});
+      HTTPProxy.performRequest("HTTPS", {}, 'endpoint', 'GET', {}, {}, null);
+      sinon.assert.calledOnce(HTTPProxy._createHTTPSRequest);
+      HTTPProxy._createHTTPSRequest.restore();
+    });
+    it('give "HTTP" should call _createHTTPRequest', function(){
+      sinon.stub(HTTPProxy, '_createHTTPRequest', function(){});
+      HTTPProxy.performRequest("HTTP", {}, 'endpoint', 'GET', {}, {}, null);
+      sinon.assert.calledOnce(HTTPProxy._createHTTPRequest);
+      HTTPProxy._createHTTPRequest.restore();
+    });
+  });
+  it('should call http.request', function(){
+    sinon.stub(http, 'request', function(args, callback){
+      return {
+        'write':function(){},
+        'end':function(){}
+      };
+    });
+    HTTPProxy._createHTTPRequest({}, "", null);
+    sinon.assert.calledOnce(http.request);
+    http.request.restore();
+  });
+  it('should call https.request', function(){
+    sinon.stub(https, 'request', function(args, callback){
+      return {
+        'write':function(){},
+        'end':function(){}
+      };
+    });
+    HTTPProxy._createHTTPSRequest({}, "", null);
+    sinon.assert.calledOnce(https.request);
+    https.request.restore();
   });
 });
