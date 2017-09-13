a816fbac697eb62a8204f2136f849918244b36e8
yushio <yushio1984@gmail.com>
2017-09-13 10:09:45 +0800

    httpClient unit test

---
+++ test/http-proxy/httpClient-test.js
@@ -0,0 +1,57 @@
+var chai = require('chai'),
+    sinon = require('sinon'),
+    sinonChai = require("sinon-chai");
+
+chai.use(sinonChai);
+var expect = chai.expect,
+    should = chai.should();
+
+var http = require('http');
+var https = require('https');
+var httpClient = require("../../src/http-proxy/httpClient");
+var Datastore = require('nedb');
+
+describe('httpClient', function(){
+  var db;
+  beforeEach(function(){
+    db = new Datastore({ filename: './proxyDB' });
+    db.loadDatabase(function (err) {});
+  });
+  
+  describe('performRequest', function(){
+    it('give "HTTPS" should call _createHTTPSRequest', function(){
+      sinon.stub(httpClient, '_createHTTPSRequest', function(){});
+      httpClient.performRequest("HTTPS", {}, 'endpoint', 'GET', {}, {}, null);
+      sinon.assert.calledOnce(httpClient._createHTTPSRequest);
+      httpClient._createHTTPSRequest.restore();
+    });
+    it('give "HTTP" should call _createHTTPRequest', function(){
+      sinon.stub(httpClient, '_createHTTPRequest', function(){});
+      httpClient.performRequest("HTTP", {}, 'endpoint', 'GET', {}, {}, null);
+      sinon.assert.calledOnce(httpClient._createHTTPRequest);
+      httpClient._createHTTPRequest.restore();
+    });
+  });
+  it('should call http.request', function(){
+    sinon.stub(http, 'request', function(args, callback){
+      return {
+        'write':function(){},
+        'end':function(){}
+      };
+    });
+    httpClient._createHTTPRequest({}, "", null);
+    sinon.assert.calledOnce(http.request);
+    http.request.restore();
+  });
+  it('should call https.request', function(){
+    sinon.stub(https, 'request', function(args, callback){
+      return {
+        'write':function(){},
+        'end':function(){}
+      };
+    });
+    httpClient._createHTTPSRequest({}, "", null);
+    sinon.assert.calledOnce(https.request);
+    https.request.restore();
+  });
+});
