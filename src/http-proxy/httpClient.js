638508d807994a13dda33583dd1b4da6c19ef96a
unclebean <unclebean2013@gmail.com>
2019-03-21 00:24:44 +0800

    fixed http request gzip content encoding issue

--- src/http-proxy/httpClient.js
+++ src/http-proxy/httpClient.js
@@ -1,6 +1,39 @@
 var https = require('https'),
     http = require('http');
 var RequestModel = require('./models/requestModel');
+var zlib = require("zlib");
+
+function gunzipRespHandler(response, callback) {
+    var gunzip = zlib.createGunzip();
+    response.pipe(gunzip);
+    var respData = '';
+    gunzip.on('data', function(data) {
+        respData += data.toString();
+    }).on("end", function() {
+        response.headers.statusCode = response.statusCode;
+        zlib.gzip(respData, function(error, data) {
+            if(error){
+                callback(error);
+            } else {
+                callback(data.toString(), response.headers);
+            }
+        });
+    }).on("error", function(e) {
+        callback(e);
+    });
+}
+
+function respHandler(response, callback) {
+    response.setEncoding('utf-8');
+    var responseString = '';
+    response.on('data', function (data) {
+        responseString += data;
+    });
+    response.on('end', function () {
+        response.headers.statusCode = response.statusCode;
+        callback(responseString, response.headers);
+    });
+}
 
 module.exports = {
     performRequest: function (requestModel, success) {
@@ -23,16 +56,11 @@ module.exports = {
     },
     _createHTTPRequest: function (options, dataString, callback) {
         var req = http.request(options, function (res) {
-            res.setEncoding('utf-8');
-            var responseString = '';
-            res.on('data', function (data) {
-                responseString += data;
-            });
-            res.on('end', function () {
-                res.headers.statusCode = res.statusCode;
-                callback(responseString, res.headers);
-            });
-
+            if(res.headers['content-encoding'] === 'gzip') {
+                gunzipRespHandler(res, callback);
+            } else {
+                respHandler(res, callback);
+            }
         });
         if (options.method !== 'GET') {
             req.write(dataString || '');
@@ -45,15 +73,11 @@ module.exports = {
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
         options.agent = new https.Agent(options);
         var req = https.request(options, function (res) {
-            res.setEncoding('utf-8');
-            var responseString = '';
-            res.on('data', function (data) {
-                responseString += data;
-            });
-            res.on('end', function () {
-                res.headers.statusCode = res.statusCode;
-                callback(responseString, res.headers);
-            });
+            if(res.headers['content-encoding'] === 'gzip') {
+                gunzipRespHandler(res, callback);
+            } else {
+                respHandler(res, callback);
+            }
         });
         if (options.method !== 'GET') {
             req.write(dataString || '');
